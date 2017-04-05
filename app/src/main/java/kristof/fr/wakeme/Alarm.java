da7e80df98ab3b5aa60e2a1739704df88cf545fa
master <kristof94.dsantos@gmail.com>
2017-04-05 09:48:35 +0200

    update test

---
+++ app/src/main/java/kristof/fr/wakeme/Alarm.java
@@ -0,0 +1,23 @@
+package kristof.fr.wakeme;
+
+import java.util.Date;
+
+/**
+ * Created by master on 4/5/17.
+ */
+
+public class Alarm {
+    private Date date;
+
+    public Alarm(Date dateToRing){
+        date = dateToRing;
+    }
+
+    public void startAlarm(){
+
+    }
+
+    public void stopAlarm(){
+
+    }
+}
